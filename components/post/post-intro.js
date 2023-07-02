511795f1cc54dc63f8dc94c86fe7686d5c6cd120
rikisommers <rikisommers@yahoo.co.nz>
2023-07-02 14:05:41 +1200

    init pure css branch

--- components/post/post-intro.js
+++ components/post/post-intro.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
 import TextAnimation from "../utils/text-animation";
-import { motion  } from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function PostIntro({ title, content }) {
   return (
@@ -11,21 +11,22 @@ export default function PostIntro({ title, content }) {
           flex items-center justify-center rounded-xl  z-30  item"
     >
       <div className="home-content gap-8">
+        <TextAnimation content={title}></TextAnimation>
 
-      <TextAnimation content={title}></TextAnimation>
-
-      <motion.p className="text-sm	text-left"
-            initial={{ opacity: 0 }}
-            animate={{
-              opacity:1
-            }}
-            transition={{
-              ease: [0.33, 1, 0.68, 1],
-              duration: 1.6,
-              delay:0.6
-            }}
-            >{content}</motion.p>
-
+        <motion.p
+          className="text-sm	text-left"
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+          }}
+          transition={{
+            ease: [0.33, 1, 0.68, 1],
+            duration: 1.6,
+            delay: 0.6,
+          }}
+        >
+          {content}
+        </motion.p>
       </div>
     </div>
   );
--- components/post/post-modal.js
+++ components/post/post-modal.js
@@ -118,7 +118,6 @@ const PostModal = ({
     }
   };
 
-
   return (
     <>
       <motion.div
