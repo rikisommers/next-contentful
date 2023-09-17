d78010fe6767fa5ebe7e4a56a892d54b18156c7a
rikisommers <rikisommers@yahoo.co.nz>
2023-09-17 20:47:33 +1200

    idy up archive tempalte

--- pages/archive.js
+++ pages/archive.js
@@ -1,15 +1,8 @@
-//npx cross-env CONTENTFUL_SPACE_ID=4v0tb3n9jpvc CONTENTFUL_MANAGEMENT_TOKEN=CFPAT-wwsdnZLZwdYpl8egGCKcVNoBv_InezP3krIyJUJACTc npm run setup
-//CFPAT-wwsdnZLZwdYpl8egGCKcVNoBv_InezP3krIyJUJACTc
-import React, { useState, useEffect, useRef, useCallback} from "react";
-import { useFrame } from '@studio-freight/hamo';
+import React, { useRef } from "react";
 
-import Container from '../components/container'
 import Layout from '../components/layout'
 import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
 import Transition from "../components/transition-wipe";
-import { useLenis } from '@studio-freight/react-lenis';
-import Lenis from '@studio-freight/lenis';
 import { ScrollableBox } from "../components/utils/scrollable";
 
 export default function Archive() {
