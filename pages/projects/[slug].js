f70d32db964e74a85897c432d633b1288520c250
rikisommers <rikisommers@yahoo.co.nz>
2024-02-20 00:24:06 +1300

    post footer parallax

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from "react";
+import { useState, useContext, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import ErrorPage from "next/error";
 import Layout from "../../components/layout";
@@ -12,6 +12,7 @@ import {
   cubicBezier,
   useMotionValue,
   useTransform,
+  useScroll,
 } from "framer-motion";
 import { ScrollableBox } from "../../components/utils/scrollable";
 
@@ -24,6 +25,8 @@ import PostTile from "../../components/post/post-tile";
 import FadeInWhenVisible from "../../components/utils/fade-in-visible";
 import NextPost from "../../components/post/post-next";
 import { RouteContext } from "../../components/routeContext";
+import Lenis from '@studio-freight/lenis'
+
 
 export default function Post({ post, nextPost }) {
   const router = useRouter();
@@ -86,9 +89,91 @@ export default function Post({ post, nextPost }) {
     return <ErrorPage statusCode={404} />;
   }
 
+
+
+  const footerRef = useRef(null);
+  const contentRef = useRef(null);
+
+
+  const [dimension, setDimension] = useState({width:0, height:0});
+
+
+
+
+
+
+  const { scrollYProgress } = useScroll({
+
+    target: footerRef,
+
+    offset: ['start end', 'end end']
+
+})
+const { height } = dimension;
+
+
+const y = useTransform(scrollYProgress, [0, 1], [-300, 0])
+const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3])
+const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25])
+const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3])
+
+
+
+useEffect( () => {
+
+
+  const lenis = new Lenis()
+
+
+
+  const raf = (time) => {
+
+    lenis.raf(time)
+    //console.log(scrollYProgress.current)
+    requestAnimationFrame(raf)
+
+  }
+
+
+
+  requestAnimationFrame(raf)
+
+
+  const resize = () => {
+
+    setDimension({width: window.innerWidth, height: window.innerHeight})
+
+  }
+
+
+
+  window.addEventListener("resize", resize)
+
+  requestAnimationFrame(raf);
+
+  resize();
+
+
+
+  return () => {
+
+    window.removeEventListener("resize", resize);
+
+  }
+
+}, [])
+
+
+
+
   return (
     <Layout>
-      <ScrollableBox onScrollChange={handleScrollChange}>
+  
+  <div
+              className="relative flex flex-col pb-20 z-50 bg-slate-100" 
+              ref={contentRef}
+            >
+
 
       {/* xxl:grid grid-cols-3 */}
         <div className="relative z-10 overflow-hidden bg-slate-50 rounded-xl ">
@@ -107,15 +192,28 @@ export default function Post({ post, nextPost }) {
             
               <PostContent post={post}></PostContent>
             </motion.div>
-            {nextPost && (
-              <div className=" p-6 ">
-                <NextPost post={nextPost} />
-              </div>
-            )}
+        
+
+  
+
       
         </div>
-        </ScrollableBox>
 
+        {nextPost && (
+                <motion.div ref={footerRef} className="testing123 relative h-vhh w-full">
+                <motion.div
+                
+                className="absolute bottom-0"
+                style={{y}}
+                // style={{ translateY: y }}
+                //  animate={{ y: footerOffset }}
+                >
+                <NextPost post={nextPost} />
+                </motion.div>
+              </motion.div>
+            )}
+
+              </div>
       {shouldFadeIn && <TransitionWipe />}
     </Layout>
   );
