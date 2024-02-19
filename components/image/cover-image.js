4c822a8d0021ed37b9bc0af1ae94639681d512cb
rikisommers <rikisommers@yahoo.co.nz>
2024-02-20 00:32:36 +1300

    cover imag paralaxx

--- components/image/cover-image.js
+++ components/image/cover-image.js
@@ -1,17 +1,54 @@
+import React, { useRef } from "react";
 import ContentfulImage from "./contentful-image";
+import {
+  motion,
+  useTransform,
+  useMotionValue,
+  cubicBezier,
+  useScroll,
+  useInView,
+} from "framer-motion";
+import RollUpWhenVisible from "../utils/roll-up-visible";
 
 export default function CoverImage({ title, url, slug, layout }) {
+  const ref = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+
+    offset: ["start end", "end start"],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-200, 0]);
+
   const image = (
-    <ContentfulImage
-      width={1920}
-      height={1280}
-      
-      sizes="(max-width: 768px) 100vw,
+    <RollUpWhenVisible>
+      <motion.div
+        ref={ref}
+        className="c-video flex flex-col align-bottom content-end"
+        // initial={{ clipPath: clipPathInitial }}
+        // animate={{ clipPath: clipPathInitial }}
+        // exit={{ clipPath: clipPathInitial }}
+        // transition={{
+        //   duration: 0.6,
+        //   easing: cubicBezier(0.35, 0.17, 0.3, 0.86),
+        // }}
+      >
+        <div className="c-video__content">
+          <motion.div className="w-full h-full py-6" style={{ y }}>
+            <ContentfulImage
+              width={1920}
+              height={1280}
+              sizes="(max-width: 768px) 100vw,
               (max-width: 1200px) 50vw,
               33vw"
-      alt={`Cover Image for ${title}`}
-      src={url}
-    />
+              alt={`Cover Image for ${title}`}
+              src={url}
+            />
+          </motion.div>
+        </div>
+      </motion.div>
+    </RollUpWhenVisible>
   );
 
   return image;
