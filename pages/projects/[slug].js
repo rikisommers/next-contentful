f024f74cbf133c8f8d7b83d10beb91690f1bbf3f
rikisommers <rikisommers@yahoo.co.nz>
2024-11-20 21:55:29 +1300

    tidy up slug remove next post and reduce max pages

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,67 +1,18 @@
-import { useState, useContext, useEffect, useRef } from "react";
-import { useRouter } from "next/navigation";
-import ErrorPage from "next/error";
 import Layout from "../../components/layout";
 import {
   getAllCaseStudies,
   getPost,
-  getNextPost,
   getFooter,
 } from "../../lib/api";
 
 import PostHeader from "../../components/post/post-header";
 
 import PostContent from "../../components/post/post-content";
-import { RouteContext } from "../../components/routeContext";
 import BlockFooter from "../../components/blocks/block-footer";
-import TransitionPage from "../../components/transition/pageTransition";
 import PageNav from "../../components/base/page-nav";
 
 export default function Post({ post, nextPost, footerData }) {
-  const router = useRouter();
-  console.log("post---------------------------", post);
 
-  const { routeInfo } = useContext(RouteContext);
-  const [destRoute, setDestRoute] = useState("");
-
-  useEffect(() => {
-    //  setSourceRoute(routeInfo.sourceRoute);
-    setDestRoute(routeInfo.destRoute);
-  }, [routeInfo]); // Include routeInfo in the dependency array if needed
-
-  const shouldFadeIn = !destRoute.includes("/projects/");
-
-  if (!router.isFallback && !post) {
-    return <ErrorPage statusCode={404} />;
-  }
-
-  //const shouldAnimate = router.paths.startsWith("/projects/");
-
-  if (!router.isFallback && !post) {
-    return <ErrorPage statusCode={404} />;
-  }
-
-  const [posT, setPosT] = useState(0);
-
-
-
-
-
-  // useEffect(() => {
-  //   if (router.pathname.startsWith("/posts/")) {
-  //     // Select the title wrapper
-  //     const next = document.querySelector(".test");
-  //     if (next) {
-  //       setPosT(Math.round(next.getBoundingClientRect().y) - 90);
-  //     }
-  //   }
-  // }, [router.pathname]);
-
-  const handleLinkClick = () => {
-    const next = document.querySelector(".test");
-    //console.log("ddd", Math.round(next.getBoundingClientRect().y) - 90);
-    setPosT(Math.round(next.getBoundingClientRect().y) - 90);
-  };
 
   return (      
       <Layout>
@@ -115,9 +66,6 @@ export default function Post({ post, nextPost, footerData }) {
              
           )}
 
-
-            
-
           {footerData && (
             <BlockFooter data={footerData} />
           )}
@@ -130,7 +78,6 @@ export default function Post({ post, nextPost, footerData }) {
 export async function getStaticProps({ params, preview = false }) {
   const slug = params.slug;
   const data = await getPost(slug, preview);
-  const next = await getNextPost(slug, preview);
   const footerData = await getFooter();
 
   return {
@@ -138,7 +85,6 @@ export async function getStaticProps({ params, preview = false }) {
       slug,
       preview,
       post: data[0] ?? null,
-      nextPost: next[0] ?? null,
       footerData: footerData || null,
     },
   };
@@ -147,7 +93,7 @@ export async function getStaticProps({ params, preview = false }) {
 export async function getStaticPaths() {
   const allPosts = await getAllCaseStudies(false);
   return {
-    paths: allPosts?.map(({ slug }) => `/projects/${slug}`) ?? [],
-    fallback: true,
+    paths: allPosts?.slice(0, 20).map(({ slug }) => `/projects/${slug}`) ?? [],
+    fallback: 'blocking',
   };
 }
