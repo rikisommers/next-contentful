c5f0850bbcd72a22508842f5a8b171ffefa50198
rikisommers <rikisommers@yahoo.co.nz>
2024-11-23 20:21:11 +1300

    hack fix build errors form app

--- appbak/new/page.js
+++ appbak/new/page.js
@@ -1,33 +1,33 @@
-// app/page.js
+// // app/page.js
 
-"use client";
+// "use client";
 
-import React, { useState, useEffect } from 'react';
-import PasswordPromptDialog from '../../components/security/password-dialog';
+// import React, { useState, useEffect } from 'react';
+// import PasswordPromptDialog from '../../components/security/password-dialog';
 
-const ProtectedPage = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+// const ProtectedPage = () => {
+//   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  useEffect(() => {
-    const checkAuth = async () => {
-      const res = await fetch('/api/check-auth');
-      if (res.status === 200) {
-        setIsAuthenticated(true);
-      }
-    };
-    checkAuth();
-  }, []);
+//   useEffect(() => {
+//     const checkAuth = async () => {
+//       const res = await fetch('/api/check-auth');
+//       if (res.status === 200) {
+//         setIsAuthenticated(true);
+//       }
+//     };
+//     checkAuth();
+//   }, []);
 
-  if (!isAuthenticated) {
-    return <PasswordPromptDialog />;
-  }
+//   if (!isAuthenticated) {
+//     return <PasswordPromptDialog />;
+//   }
 
-  return (
-    <div>
-      <h1>Protected Content</h1>
-      <p>This content is protected by a password.</p>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>Protected Content</h1>
+//       <p>This content is protected by a password.</p>
+//     </div>
+//   );
+// };
 
-export default ProtectedPage;
+// export default ProtectedPage;
