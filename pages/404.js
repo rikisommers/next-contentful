bda7f68a0db29afd3fcb913d752db95a38dc42d7
rikisommers <rikisommers@yahoo.co.nz>
2024-11-23 20:38:43 +1300

    404 test

--- pages/404.js
+++ pages/404.js
@@ -1,24 +1,18 @@
 // pages/_error.js
 
-import React, { Suspense } from 'react';
-import { useSearchParams } from 'next/navigation';
+import React from 'react';
 import Layout from "../components/layout";
 
-const NotFoundComponent = () => {
-    const searchParams = useSearchParams();
 
-    // Your logic using searchParams
-    return <h1>404 - Page Not Found</h1>;
-};
 
 export default function Custom404() {
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Layout>
-                <div className='flex items-center justify-center w-full h-full'>
-                    <NotFoundComponent />
-                </div>
-            </Layout>
-        </Suspense>
-    );
-}
+    return  (
+        <Layout>
+          <div className='flex items-center justify-center w-full h-full'>
+          <h1>
+            404
+          </h1>
+          </div>
+        </Layout>
+      );
+  }
